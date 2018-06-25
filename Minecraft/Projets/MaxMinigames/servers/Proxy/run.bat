152d8ae669fed177688c9201875e5550449d7a10
Maxime3399 <maxime3399.minecraft@gmail.com>
2018-06-25 18:48:01 +0200

    25/06/2018 Save 1

--- Minecraft/Projets/MaxMinigames/servers/Proxy/run.bat
+++ Minecraft/Projets/MaxMinigames/servers/Proxy/run.bat
@@ -1,5 +1,5 @@
 @ECHO OFF
 SET BINDIR=%~dp0
 CD /D "%BINDIR%"
-"C:\Program Files\Java\jre1.8.0_162\bin\java.exe" -Xmx512M -Xms512M -jar BungeeCord.jar
+"C:\Program Files\Java\jre1.8.0_171\bin\java.exe" -Xmx512M -Xms512M -jar BungeeCord.jar
 PAUSE
