cc0525b742f1e786f915f8701b604494ce5bbcca
Maxime3399 <maxime3399.minecraft@gmail.com>
2018-08-26 10:57:49 +0200

    26/08/2018 Save 1

--- Minecraft/Projets/M-Cube/run.bat
+++ Minecraft/Projets/M-Cube/run.bat
@@ -1,5 +1,5 @@
 @ECHO OFF
 SET BINDIR=%~dp0
 CD /D "%BINDIR%"
-"C:\Program Files\Java\jre1.8.0_181\bin\java.exe" -Xmx1024M -Xms1024M -jar spigot-1.13.jar
+"C:\Program Files\Java\jre1.8.0_181\bin\java.exe" -Xmx1024M -Xms1024M -jar spigot-1.13.1.jar
 PAUSE
